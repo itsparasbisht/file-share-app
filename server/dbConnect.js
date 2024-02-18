4d91f87b3bf67c6780ba3a8bd96bafa3f2c8e3e7
itsparasbisht <iamparasbisht@gmail.com>
2024-02-18 20:21:11 +0530

    es lint setup

---
+++ server/dbConnect.js
@@ -0,0 +1,9 @@
+mongoose.connection.on("open", function () {
+  console.log("Connected to mongoDB");
+});
+
+mongoose.connection.on("error", function () {
+  console.log("Could not connect to mongoDB");
+});
+
+mongoose.connect(process.env.DATABASE_URL);
--- server/server.js
+++ server/server.js
@@ -27,16 +27,6 @@ const bucket = storage.bucket(admin.storage().bucket().name);
 
 const upload = multer({ dest: "uploads" });
 
-mongoose.connection.on("open", function () {
-  console.log("Connected to mongoDB");
-});
-
-mongoose.connection.on("error", function () {
-  console.log("Could not connect to mongoDB");
-});
-
-mongoose.connect(process.env.DATABASE_URL);
-
 app.get("/", (req, res) => {
   res.json({ message: "root" });
 });
