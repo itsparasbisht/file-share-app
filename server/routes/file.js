006327ae44b26e2df387555fc1d03ed40c1f01f7
itsparasbisht <iamparasbisht@gmail.com>
2024-02-20 19:49:55 +0530

    post-upload api added

--- server/routes/file.js
+++ server/routes/file.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const { v4: uuidv4 } = require("uuid");
 
 const admin = require("firebase-admin");
 const { Storage } = require("@google-cloud/storage");
 const serviceAccount = require("../serviceAccount");
+const File = require("../models/File");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -30,27 +32,33 @@ router.get("/generate-upload-url", (req, res) => {
         return res.status(500).send("Error generating upload URL");
       }
 
-      res.status(200).json({ uploadUrl: url });
+      res.status(200).json({ uploadUrl: url, fileID: uuidv4() });
     }
   );
 });
 
 router.post("/post-upload", async (req, res) => {
-  const fileData = {
-    fileID: req.body.id,
-    filename: req.body.filename,
-  };
+  try {
+    const fileData = {
+      fileID: req.body.fileID,
+      filename: req.body.filename,
+    };
 
-  if (req.body.password !== undefined && req.body.password !== "") {
-    fileData.password = await bcrypt.hash(req.body.password, 10);
+    if (req.body.password !== undefined && req.body.password !== "") {
+      fileData.password = await bcrypt.hash(req.body.password, 10);
+    }
+
+    const file = await File.create(fileData);
+
+    res.json({
+      filename: file.filename,
+      fileID: file.fileID,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "something went wrong", error: error.message });
   }
-
-  const file = await File.create(fileData);
-
-  res.json({
-    filename: file.originalName,
-    link: `${req.headers.host}/file/${file.id}`,
-  });
 });
 
 module.exports = router;
