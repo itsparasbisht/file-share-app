02c0d77593f7b5cbcd99a1dcfa1ad981220f80bb
itsparasbisht <iamparasbisht@gmail.com>
2024-02-25 12:38:17 +0530

    cors setup

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 require("./dbConnect");
+const cors = require("cors");
+
+const allowedOrigins = ["http://localhost:3000"];
 
 const express = require("express");
 const app = express();
@@ -10,6 +13,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const PORT = process.env.PORT;
+
+// cors setup
+app.use(
+  cors({
+    origin:
+      process.env.INSTANCE === "local"
+        ? "*"
+        : function (origin, callback) {
+            if (allowedOrigins.includes(origin)) {
+              callback(null, true);
+            } else {
+              callback(new Error("Not allowed by CORS"));
+            }
+          },
+  })
+);
 
 app.get("/", (req, res) => {
   res.json({ message: "root" });
